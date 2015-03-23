8e76deb0eaf5bf8a22b05b95672e62f167f571f4
carlosandresbastos <carlosbastos@yahoo.com>
2015-03-22 21:54:31 -0500

    Initial Version
    Initial Version of MVC Git

---
+++ WebGitRepositories/MvcGitRepositories/MvcGitRepositories/Controllers/RepositoriesController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcGitRepositories.Controllers
+{
+    public class RepositoriesController : Controller
+    {
+        //
+        // GET: /MyRepositories/
+
+        String strURLGetRepositoriesByUser;
+        String strURLSearchRepositories;
+
+        String _strMessage;
+
+        // Variable to store Message if exists any problem
+        public String StrMessage
+        {
+            get { return _strMessage; }
+            set { _strMessage = value; }
+        }
+
+        public static bool ValidateServerCertificate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            return true;
+        }
+
+        public RepositoriesController()
+        {
+            strURLGetRepositoriesByUser = " https://api.github.com/users/{0}/repos";
+            strURLSearchRepositories = "https://api.github.com/search/repositories?q={0}&sort={1}&order={2}";
+
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult MyRepositories()
+        {
+            
+            ViewBag.Message = "Repositórios do Carlos Bastos.";
+            
+
+            return View();
+        }
+
+        public String GetMyRepositories()
+        {
+
+            
+            // Attach user for the repository
+            String strUrlWebService = String.Format(strURLGetRepositoriesByUser, "carlosandresbastos");
+            String strResponse = GET(strUrlWebService);
+
+            
+            return strResponse;
+        }
+
+        public String SearchRepositories(String strWord)
+        {
+
+            
+            // Attach Position to search to the URL
+            String strUrlWebService = String.Format(strURLSearchRepositories, strWord);
+            String strResponse = GET(strUrlWebService);
+
+
+            return strResponse;
+        }
+
+
+
+
+        // Returns JSON string
+        string GET(string url)
+        {
+            string s;
+            using (WebClient client = new WebClient())
+            {
+                string ua = "User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; rv:8.0) Gecko/20100101 Firefox/8.0";
+                client.Headers.Add(HttpRequestHeader.UserAgent, ua);
+                client.UseDefaultCredentials = true;
+                s = client.DownloadString(url);
+            }
+            return s;
+        }
+
+    }
+}
---
+++ WebGitRepositories/MvcGitRepositories/MvcGitRepositories/Models/RepositoryModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MvcGitRepositories.Controllers;
+
+namespace MvcGitRepositories.Models
+{
+    public class RepositoryModel
+    {
+        public String GetRepositoriesbyUser()
+        {
+            RepositoriesController objRep = new RepositoriesController();
+            return objRep.GetMyRepositories();
+            
+        }
+    }
+}
